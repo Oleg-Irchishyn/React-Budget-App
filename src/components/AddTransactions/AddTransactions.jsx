eabddb4db7d439dfab6ba37e35cbb63247c796bd
Oleg <oleg.irchishyn91@gmail.com>
2020-07-09 20:15:10 +0300

    fixed validation problem

--- src/components/AddTransactions/AddTransactions.jsx
+++ src/components/AddTransactions/AddTransactions.jsx
@@ -3,27 +3,14 @@ import { Field, reduxForm } from 'redux-form';
 import { Input } from '../common/FormControls/FormControls';
 import { maxLengthCreator, required } from '../../redux/utils/validators/validatos';
 
-const AddTransactions = (props) => {
-  const onIncomeFormSubmit = (formData) => {
-    console.log(formData.Income, formData.IncomeAmount)
-  }
-  const onExpensesFormSubmit = (formData) => {
-    console.log(formData.Expenses, formData.ExpensesAmount)
-  }
-  return (
-    <div className="form-wrapper">
-      <IncomeReduxForm onSubmit={onIncomeFormSubmit} />
-      <ExpensesReduxForm onSubmit={onExpensesFormSubmit} />
-    </div>
-  )
-};
+
+const IncomeFormvalidators = [required, maxLengthCreator(100)];
 
 const IncomeForm = React.memo(({ handleSubmit }) => {
-  const maxLength15 = maxLengthCreator(15)
   return (
     <form onSubmit={handleSubmit}>
       <div className="input-group income">
-        <Field component={Input} name="Income" placeholder="Add Income..." type="text" autoComplete="off" validate={required} />
+        <Field component={Input} name="Income" placeholder="Add Income..." type="text" autoComplete="off" validate={IncomeFormvalidators} />
         <Field component={Input} name="IncomeAmount" placeholder="Amount" type="number" autoComplete="off" />
         <button type="submit">Submit</button>
       </div>
@@ -31,12 +18,13 @@ const IncomeForm = React.memo(({ handleSubmit }) => {
   )
 })
 
+const ExpenseFormvalidators = [required, maxLengthCreator(100)];
+
 const ExpensesForm = React.memo(({ handleSubmit }) => {
-  const maxLength15 = maxLengthCreator(15)
   return (
     <form onSubmit={handleSubmit}>
       <div className="input-group expense">
-        <Field component={Input} name="Expenses" placeholder="Add Expense..." type="text" autoComplete="off" validate={required} />
+        <Field component={Input} name="Expenses" placeholder="Add Expense..." type="text" autoComplete="off" validate={ExpenseFormvalidators} />
         <Field component={Input} name="ExpensesAmount" placeholder="Amount" type="number" autoComplete="off" />
         <button type="submit">Submit</button>
       </div>
@@ -52,6 +40,21 @@ const IncomeReduxForm = reduxForm({
 const ExpensesReduxForm = reduxForm({
   form: "expenses"
 })(ExpensesForm);
+
+const AddTransactions = (props) => {
+  const onIncomeFormSubmit = (formData) => {
+    console.log(formData.Income, formData.IncomeAmount)
+  }
+  const onExpensesFormSubmit = (formData) => {
+    console.log(formData.Expenses, formData.ExpensesAmount)
+  }
+  return (
+    <div className="form-wrapper">
+      <IncomeReduxForm onSubmit={onIncomeFormSubmit} />
+      <ExpensesReduxForm onSubmit={onExpensesFormSubmit} />
+    </div>
+  )
+};
 
 
 export default AddTransactions;
